460f0c8d416f5260c21835f03d0e23275d109ce7
Andres Weber <andresmweber@gmail.com>
2017-11-08 12:26:46 -0500

    Stu and I working and non-functional code added.  Needs to be refactored as discussed.

--- anvil/plugins/maya/__init__.py
+++ anvil/plugins/maya/__init__.py
@@ -5,9 +5,8 @@ def is_dcc_loaded():
     except ImportError:
         return False
 
-def lazy_import():
-    import create
-    import scene
-    import connections
-    import dependencies
-    return dependencies.API
+
+import create
+import scene
+import connections
+import dependencies
