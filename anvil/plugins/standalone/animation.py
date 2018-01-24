df4ca6ceb890d9cadd0a05f1ce6c0b2e66cd8342
Andres Weber <andresmweber@gmail.com>
2018-01-24 16:01:55 -0500

    Added libpng.

---
+++ anvil/plugins/standalone/animation.py
@@ -0,0 +1,2 @@
+def set_keyframe(*objects, **kwargs):
+    pass
--- anvil/plugins/standalone/connections.py
+++ anvil/plugins/standalone/connections.py
@@ -1,3 +1,67 @@
+def connected_attr(attribute_dag_path_1, attribute_dag_path_2, **kwargs):
+    pass
+
+
+def mute(attribute_dag_path, **kwargs):
+    pass
+
+
+def dirty_attr(attribute_dag_path, **kwargs):
+    pass
+
+
+def connection_info(attribute_dag_path, **kwargs):
+    pass
+
+
+def info_attr(attribute_dag_path, **kwargs):
+    pass
+
+
+def list_history(dag_path, **kwargs):
+    pass
+
+
+def list_connections(attribute_dag_path, **kwargs):
+    pass
+
+
+def alias_attr(attribute_dag_path, **kwargs):
+    pass
+
+
+def query_attr(attribute, **kwargs):
+    pass
+
+
+def list_attr(node, *args, **kwargs):
+    pass
+
+
+def delete_attr(attribute_dag_path_or_node, **kwargs):
+    pass
+
+
+def get_attr(attribute_dag_path, *args, **kwargs):
+    pass
+
+
+def set_attr(attribute, **kwargs):
+    pass
+
+
+def add_attr(attribute, **kwargs):
+    pass
+
+
+def connect_attr(source_attribute, destination_attribute, **kwargs):
+    pass
+
+
+def disconnect_attr(*attributes, **kwargs):
+    pass
+
+
 def translate(source, targets, **flags):
     return True
 
