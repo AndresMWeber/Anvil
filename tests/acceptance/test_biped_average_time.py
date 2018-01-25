0551ee434b7dd296d791d6ffbbbd7ffe8dbb3e5a
andresweber <andresmweber@gmail.com>
2018-01-25 16:04:56 -0500

    Added try excepts for tests that are supposed to be just local.

--- tests/acceptance/test_biped_average_time.py
+++ tests/acceptance/test_biped_average_time.py
@@ -1,22 +1,26 @@
 from tests.base_test import cleanup_nodes
 import test_build_biped
-from pycallgraph import PyCallGraph
 from pycallgraph.output import GraphvizOutput
-import cProfile as profile
-import unittest
 
 
 class TestProfileBiped(test_build_biped.TestBaseTemplateRigs):
-    @unittest.skip
     def test_pycall_graph(self):
-        with cleanup_nodes():
-            with PyCallGraph(output=GraphvizOutput()):
-                self.from_template_file(self.TPOSE)
+        try:
+            from pycallgraph import PyCallGraph
+            with cleanup_nodes():
+                with PyCallGraph(output=GraphvizOutput()):
+                    self.from_template_file(self.TPOSE)
+        except ImportError:
+            pass
 
-    @unittest.skip
     def test_cprofiler(self):
-        with cleanup_nodes():
-            profile.runctx('self.from_template_file(self.TPOSE)', globals(), locals())
+        try:
+            import cProfile as profile
+
+            with cleanup_nodes():
+                profile.runctx('self.from_template_file(self.TPOSE)', globals(), locals())
+        except ImportError:
+            pass
 
     # def test_cprofiler_quiet_logging(self):
     #    with cleanup_nodes():
